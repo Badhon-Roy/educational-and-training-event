b54ff89b77844e65f01999c6ee04509be68bbf3b
Badhon-Roy <roybadhon286@gmail.com>
2023-10-09 23:07:16 +0600

    just mediem device responsive some change

--- src/Pages/Home/Services.jsx
+++ src/Pages/Home/Services.jsx
@@ -16,7 +16,7 @@ const Services = () => {
                 data-aos-easing="ease-in-sine">
                 <h2 className="md:text-4xl text-2xl text-center font-semibold md:mt-32 mt-16 md:mb-16 mb-8" >Our Services <span className="w-[60px] mt-1P h-[5px]  mx-auto border-b-[5px] block border-orange-500"></span> </h2>
             </div>
-            <div className="grid md:grid-cols-3 grid-cols-1 gap-4 mb-16">
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 mb-16">
                 {
                     services.map(service => <Service key={service.id} service={service}></Service>)
                 }
